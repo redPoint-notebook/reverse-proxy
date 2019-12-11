e04954d4d6bb451a1372764eb914d659dda4a12f
Charles Ging <charles.ging@gmail.com>
2019-12-10 18:55:30 -0600

    update worker

--- redisWorker.js
+++ redisWorker.js
@@ -5,16 +5,18 @@ const db = require("./db");
 const intId = setInterval(() => {
   // if anything in queue, process it using BLPOP - blocking list left pop
   redisClient.blpop("webhookqueue", 0, (err, msg) => {
-    if (!msg) {
-      clearInterval(intId);
-      redisClient.quit();
+    // if (!msg) {
+    //   clearInterval(intId);
+    //   redisClient.quit();
+    // }
+    if (msg) {
+      let data = JSON.parse(msg[1]);
+      let notebookId = Object.keys(data)[0];
+      let webhookData = data[notebookId];
+
+      console.log("webhookqueue msg : ", msg);
+
+      db("WEBHOOK", null, notebookId, webhookData);
     }
-    let data = JSON.parse(msg[1]);
-    let notebookId = Object.keys(data)[0];
-    let webhookData = data[notebookId];
-
-    console.log("webhookqueue msg : ", msg);
-
-    db("WEBHOOK", null, notebookId, webhookData);
   });
 }, 100);

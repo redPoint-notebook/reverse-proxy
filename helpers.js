a9027a5fbd1c1c0bb0619531c75dd608501d39ec
Charles Ging <charles.ging@gmail.com>
2019-12-10 18:05:03 -0600

    redis worker file

--- helpers.js
+++ helpers.js
@@ -180,24 +180,24 @@ const enqueueWebhookData = (req, res) => {
   });
 };
 
-const processWebhookData = () => {
-  const intId = setInterval(() => {
-    // if anything in queue, process it using BLPOP - blocking list left pop
-    redisClient.blpop("webhookqueue", 0, (err, msg) => {
-      if (!msg) {
-        clearInterval(intId);
-        redisClient.quit();
-      }
-      let data = JSON.parse(msg[1]);
-      let notebookId = Object.keys(data)[0];
-      let webhookData = data[notebookId];
+// const processWebhookData = () => {
+//   const intId = setInterval(() => {
+//     // if anything in queue, process it using BLPOP - blocking list left pop
+//     redisClient.blpop("webhookqueue", 0, (err, msg) => {
+//       if (!msg) {
+//         clearInterval(intId);
+//         redisClient.quit();
+//       }
+//       let data = JSON.parse(msg[1]);
+//       let notebookId = Object.keys(data)[0];
+//       let webhookData = data[notebookId];
 
-      console.log("webhookqueue msg : ", msg);
+//       console.log("webhookqueue msg : ", msg);
 
-      db("WEBHOOK", null, notebookId, webhookData);
-    });
-  }, 100);
-};
+//       db("WEBHOOK", null, notebookId, webhookData);
+//     });
+//   }, 100);
+// };
 
 // const saveWebhook = (req, res) => {
 //   const matchData = req.url.match(/\/webhooks\/(.*)/);
---
+++ redisWorker.js
@@ -0,0 +1,19 @@
+const redis = require("redis");
+const redisClient = redis.createClient();
+
+const intId = setInterval(() => {
+  // if anything in queue, process it using BLPOP - blocking list left pop
+  redisClient.blpop("webhookqueue", 0, (err, msg) => {
+    if (!msg) {
+      clearInterval(intId);
+      redisClient.quit();
+    }
+    let data = JSON.parse(msg[1]);
+    let notebookId = Object.keys(data)[0];
+    let webhookData = data[notebookId];
+
+    console.log("webhookqueue msg : ", msg);
+
+    db("WEBHOOK", null, notebookId, webhookData);
+  });
+}, 100);

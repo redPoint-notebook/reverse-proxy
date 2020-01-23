2207edc2bdcaebd197e66022512805dbc9232956
William Mills <millswilliamrobert@gmail.com>
2020-01-23 10:44:31 -0500

    use regex instead of string literal

--- helpers.js
+++ helpers.js
@@ -148,7 +148,7 @@ const startNewSession = (req, res) => {
   });
   const sessionId = uuidv4().slice(0, 6);
   const sessionURL = `${sessionId}.${ROOT_WITHOUT_SUBDOMAIN}`;
-  const interpolatedHtml = html.replace("${}", `${sessionURL}`);
+  const interpolatedHtml = html.replace(/\$\{\}/g, `${sessionURL}`);
 
   res.end(interpolatedHtml);
 

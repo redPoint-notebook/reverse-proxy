d750ada0d205a954eeca2bbcdf2427d351b40173
Charles Ging <charles.ging@gmail.com>
2020-01-30 12:17:05 -0700

    revert some changes

--- proxyServer.js
+++ proxyServer.js
@@ -112,9 +112,8 @@ const proxyServer = https.createServer(https_options, (req, res) => {
             // Use Dockerode to check container health :
             if (docker.getContainer(sessionData.containerId)) {
               console.log(`Headers: ${JSON.stringify(res.getHeaders())}`);
-              // res.writeHead(200);
-              // res.send();
-              res.sendStatus(200);
+              res.writeHead(200);
+              res.end();
               // helpers.log("DOCKER CONTAINER : ");
               // helpers.log(docker.getContainer(sessionData.containerId));
             } else {
